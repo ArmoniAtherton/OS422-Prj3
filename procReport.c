68d9ceb9d34bfce75f32bf98e5f5b64ea6444f09
ArmoniAtherton <athera@uw.edu>
2018-12-02 17:30:00 -0800

    Reading physical adress

--- procReport.c
+++ procReport.c
@@ -9,12 +9,43 @@ Armoni Atherton, Joshua Atherton
 
 #include <linux/kernel.h>
 #include <linux/sched/signal.h>
+#include <asm/pgtable.h>
+
 // #include <linux/mm_types.h>
 
-long virt2phys(struct mm_struct * mm, unsigned long vpage) {
-  printk("YES!!");
-  return 0;
+unsigned long virt2phys(struct mm_struct * mm, unsigned long vpage) {
+  pgd_t *pgd;
+  p4d_t *p4d;
+  pud_t *pud;
+  pmd_t *pmt;
+  pte_t *pte;
+  struct page *page;
 
+  pgd = pgd_offset(mm, vpage);
+  if (pgd_none(*pgd) || pgd_bad(*pgd))
+    return 0;
+
+  p4d = p4d_offset(pgd, vpage);
+  if (p4d_none(*p4d) || p4d_bad(*p4d))
+    return 0;
+
+  pud = pud_offset(p4d, vpage);
+  if (pud_none(*pud) || pud_bad(*pud))
+    return 0;
+  //?!?!?!?!?!?! NOT SURE IF IT SHOULD BE UNSINED LONG
+   pmt = pmd_offset(pud, vpage);
+  if (pmd_none(*pmt) || pmd_bad(*pmt))
+    return 0;
+
+  if (!(pte = pte_offset_map(pmt, vpage)))
+    return 0;
+
+  if (!(page = pte_page(*pte)))
+    return 0;
+
+  unsigned long physical_page_addr = page_to_phys(page);
+  pte_unmap(pte);
+  return physical_page_addr;
 }
 int proc_init (void) {
   // printk(KERN_INFO "helloModule: kernel module initialized\n");
@@ -22,7 +53,7 @@ int proc_init (void) {
     for_each_process(task) {
       //Check vaild process.
       if (task->pid > 650) {
-        printk("%s [%d]\n",task->comm , task->pid);
+        // printk("%s [%d]\n",task->comm , task->pid);
 
         struct vm_area_struct *vma = 0;
         unsigned long vpage;
@@ -30,11 +61,8 @@ int proc_init (void) {
           for (vma = task->mm->mmap; vma; vma = vma->vm_next)
             for (vpage = vma->vm_start; vpage < vma->vm_end; vpage += PAGE_SIZE) {
               unsigned long phys = virt2phys(task->mm, vpage);
+              printk("This is physical adress: %lu \n", phys);
             }
-              // unsigned long phys = virt2phys(task->mm, vpage);
-              // printk("YES!");
-
-
         }
           // if (task->mm && task->mm->mmap) 
           //     for (vma = task->mm->mmap; vma; vma = vma->vm_next)
