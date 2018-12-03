e383575a8299f39e17ec10940994ee291f3d437f
ArmoniAtherton <athera@uw.edu>
2018-12-02 16:54:53 -0800

    Implemented task struct and page walking

--- procReport.c
+++ procReport.c
@@ -7,8 +7,43 @@ Armoni Atherton, Joshua Atherton
 // #include <linux/kernel.h>	/* Needed for KERN_INFO */
 // #include <linux/init.h>		/* Needed for the macros */
 
+#include <linux/kernel.h>
+#include <linux/sched/signal.h>
+// #include <linux/mm_types.h>
+
+long virt2phys(struct mm_struct * mm, unsigned long vpage) {
+  printk("YES!!");
+  return 0;
+
+}
 int proc_init (void) {
-  printk(KERN_INFO "helloModule: kernel module initialized\n");
+  // printk(KERN_INFO "helloModule: kernel module initialized\n");
+  struct task_struct *task;
+    for_each_process(task) {
+      //Check vaild process.
+      if (task->pid > 650) {
+        printk("%s [%d]\n",task->comm , task->pid);
+
+        struct vm_area_struct *vma = 0;
+        unsigned long vpage;
+        if (task->mm && task->mm->mmap) {
+          for (vma = task->mm->mmap; vma; vma = vma->vm_next)
+            for (vpage = vma->vm_start; vpage < vma->vm_end; vpage += PAGE_SIZE) {
+              unsigned long phys = virt2phys(task->mm, vpage);
+            }
+              // unsigned long phys = virt2phys(task->mm, vpage);
+              // printk("YES!");
+
+
+        }
+          // if (task->mm && task->mm->mmap) 
+          //     for (vma = task->mm->mmap; vma; vma = vma->vm_next)
+          //         for (vpage = vma->vm_start; vpage < vma->vm_end; vpage += PAGE_SIZE)
+          //             // unsigned long phys = virt2phys(task->mm, vpage);
+          
+        }
+      // printk("%s [%d]\n",task->comm , task->pid);
+    }
   return 0;
 }
 
@@ -32,3 +67,10 @@ sudo insmod ./procReport.ko
 Do "make clean" before pushing to git-hub
 
 */
+
+// struct vm_area_struct *vma = 0;
+// unsigned long vpage;
+  // if (task->mm && task->mm->mmap)
+  //     for (vma = task->mm->mmap; vma; vma = vma->vm_next)
+  //         for (vpage = vma->vm_start; vpage < vma->vm_end; vpage += PAGE_SIZE)
+  //             unsigned long phys = virt2phys(task->mm, vpage);
