af2dddce11ea71ec527d9bae7aa8d065cc3cb21a
Joshua <joshua9@uw.edu>
2018-12-03 19:36:36 -0800

    writes to proc file

--- procReport.c
+++ procReport.c
@@ -10,18 +10,20 @@ To remove a previously installed the module:
 sudo rmmod ./procReport.ko
 
 To install a newly built module:
-sudo insmod ./procReport.ko  
+sudo insmod ./procReport.ko   
 
 Do "make clean" before pushing to git-hub
 */
 
 #include <linux/module.h>	/* Needed by all modules */
-// #include <linux/kernel.h>	/* Needed for KERN_INFO */
+#include <linux/kernel.h>	/* Needed for KERN_INFO */
 // #include <linux/init.h>		/* Needed for the macros */
 
-#include <linux/kernel.h>
 #include <linux/sched/signal.h>
 #include <asm/pgtable.h>
+
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
 
 // #include <linux/mm_types.h>
 
@@ -31,59 +33,71 @@ MODULE_DESCRIPTION("Count the segmentation of the pages memorry.");
 MODULE_VERSION("1.0");
 
 /* ***** Prototypes ******** */
-int proc_init (void);
-unsigned long virt2phys(struct mm_struct * mm, unsigned long vpage);
-void proc_cleanup(void);
+static int proc_init (void);
+static unsigned long virt2phys(struct mm_struct * mm, unsigned long vpage);
+static void proc_cleanup(void);
+//proc file functions
+static int proc_report_show(struct seq_file *m, void *v);
+static int proc_report_open(struct inode *inode, struct  file *file);
 
-/* ***** Counter struct **** */ 
+/* ***** structs **** */ 
 typedef struct _counter {
   int contig_pages;
   int noncontig_pages;
   int total_pages;
 } stats_counter;
 
+static const struct file_operations proc_report_fops = {
+  .owner = THIS_MODULE,
+  .open = proc_report_open,
+  .read = seq_read,
+  .llseek = seq_lseek,
+  .release = single_release,
+};
+
 //todo: delete comment below
 // wanted fields example: proc_id,proc_name,contig_pages,noncontig_pages,total_pages 
 
 /* ***** Global values **** */ 
-static stats_counter *pages_counter;
+ stats_counter pages_counter =  {
+   .contig_pages = 0,
+   .noncontig_pages = 0,
+   .total_pages = 0
+ };
 
 /* ***** Functions For the kernel ***************** */ 
 /**
  * Inialize  and start the kernal task.
  */
-int proc_init (void) {
+static int proc_init (void) {
   // printk(KERN_INFO "helloModule: kernel module initialized\n");
   struct task_struct *task;
-    for_each_process(task) {
-      //Check vaild process.
-      if (task->pid > 650) {
-        // printk("%s [%d]\n",task->comm , task->pid);
+  proc_create("proc_report", 0, NULL, &proc_report_fops);
+  for_each_process(task) {
+    //Check vaild process.
+    if (task->pid > 650) {
+      // this prints the name and PID of each task
+      printk("%s [%d]\n",task->comm , task->pid);
 
-        struct vm_area_struct *vma = 0;
-        unsigned long vpage;
-        if (task->mm && task->mm->mmap) {
-          for (vma = task->mm->mmap; vma; vma = vma->vm_next)
-            for (vpage = vma->vm_start; vpage < vma->vm_end; vpage += PAGE_SIZE) {
-              unsigned long phys = virt2phys(task->mm, vpage);
-              printk("This is physical adress: %lu \n", phys);
-            }
-        }
-          // if (task->mm && task->mm->mmap) 
-          //     for (vma = task->mm->mmap; vma; vma = vma->vm_next)
-          //         for (vpage = vma->vm_start; vpage < vma->vm_end; vpage += PAGE_SIZE)
-          //             // unsigned long phys = virt2phys(task->mm, vpage);
-          
-        }
-      // printk("%s [%d]\n",task->comm , task->pid);
+      struct vm_area_struct *vma = 0;
+      unsigned long vpage;
+      if (task->mm && task->mm->mmap) {
+        for (vma = task->mm->mmap; vma; vma = vma->vm_next)
+          for (vpage = vma->vm_start; vpage < vma->vm_end; vpage += PAGE_SIZE) {
+            unsigned long phys = virt2phys(task->mm, vpage);
+            //printk("This is physical adress: %lu \n", phys);
+          }
+      }          
     }
+    // printk("%s [%d]\n",task->comm , task->pid);
+  }
   return 0;
 }
 
 /**
  * Get the mapping of virtual to physical memory addresses.
  */
-unsigned long virt2phys(struct mm_struct * mm, unsigned long vpage) {
+static unsigned long virt2phys(struct mm_struct * mm, unsigned long vpage) {
   pgd_t *pgd;
   p4d_t *p4d;
   pud_t *pud;
@@ -121,12 +135,28 @@ unsigned long virt2phys(struct mm_struct * mm, unsigned long vpage) {
 /**
  * Finish and end the kernal task.
  */
-void proc_cleanup(void) {
+static void proc_cleanup(void) {
+  remove_proc_entry("proc_report", NULL);
   printk(KERN_INFO "helloModule: performing cleanup of module\n");
 }
 
+/**
+ * write 
+ */
+static int proc_report_show(struct seq_file *m, void *v) {
+  seq_printf(m, "Hello proc!\n");
+  return 0;
+}
 
-/* ******** Kernal functions ************************ */
+/**
+ * Open the proc file
+ */
+static int proc_report_open(struct inode *inode, struct  file *file) {
+  return single_open(file, proc_report_show, NULL);
+}
+
+
+/* ******** Kernal Run functions ************************ */
 module_init(proc_init);
 module_exit(proc_cleanup);
 
